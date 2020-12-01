c829d8382137a035813f61481157b96d3a860e8f
Barnicle <egalimov95@gmail.com>
2020-12-01 19:23:42 +0300

    add templates and mock data

--- src/view/tripPrice.js
+++
@@ -1,7 +0,0 @@
-export const tripPriceTemplate = (data) => {
-  let Totalprice = 0;
-  data.map((el) => Totalprice += el.price);
-  return `<p class="trip-info__cost">
-              Total: &euro;&nbsp;<span class="trip-info__cost-value">${Totalprice}</span>
-            </p>`;
-};
